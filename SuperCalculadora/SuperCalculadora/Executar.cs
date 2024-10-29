4be37c81c2ac89563bc245980306260762a99531
dennerborba <dennerdeborba@gmail.com>
2024-10-28 21:51:52 -0300

    add projeto + operacao soma

---
+++ SuperCalculadora/SuperCalculadora/Executar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCalculadora
+{
+
+    public class Somar
+    {
+        public int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+
+
+
+    public class Executar
+    {
+        public static void Executor()
+        {
+            var soma = new Somar();
+            Console.WriteLine(soma.Soma(20, 30));
+        }
+    }
+}
