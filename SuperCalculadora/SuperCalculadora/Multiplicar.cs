dffc946c15edc44dd28dcdb2145181541ae14d1e
Duduzyn-py <eduardocaitano15@gmail.com>
2024-10-30 21:49:01 -0300

    Arrumei a classe multiplicação

--- SuperCalculadora/SuperCalculadora/Multiplicar.cs
+++ SuperCalculadora/SuperCalculadora/Multiplicar.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace SuperCalculadora
 {
-    public class Multiplicar
+    public class Multiplicar : IOperacao
     {
 
-        public int Multiplicacao(int n1, int n2)
+        public int Multiplicacao()
         {
-            return n1 * n2;
+            int resultado = 0;
+            int n1;
+            int n2;
+            Console.WriteLine("Digite o primeiro numero: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            n2 = int.Parse(Console.ReadLine());
+            resultado = n1 * n2;
+            return resultado;
         }
     }
 }
