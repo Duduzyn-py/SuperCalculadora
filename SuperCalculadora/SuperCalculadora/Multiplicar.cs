4f16faf4194ecf46e11f58b84b372dd86a636851
Eduardo Caitano <83654560+Duduzyn-py@users.noreply.github.com>
2024-11-03 17:50:07 -0300

    ajustando a classe dividir, commit pelo github, porque meu notebook não ta aguentando rodar o git e o Visual Studio

--- SuperCalculadora/SuperCalculadora/Multiplicar.cs
+++ SuperCalculadora/SuperCalculadora/Multiplicar.cs
@@ -14,10 +14,22 @@ namespace SuperCalculadora
             int resultado = 0;
             int n1;
             int n2;
+
             Console.WriteLine("Digite o primeiro numero: ");
             n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite o segundo numero: ");
             n2 = int.Parse(Console.ReadLine());
+            
+            if (n1  == 0)
+            {
+             Console.WriteLine("Multiplição por zero resulta em zero");
+            }
+            
+            if (n2 == 0) 
+            { 
+             Console.WriteLine("Multiplição por zero resulta em zero");
+            }
+            
             resultado = n1 * n2;
             return resultado;
         }
