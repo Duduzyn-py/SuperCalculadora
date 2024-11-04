227044961cae7bd9973ec6e848e73748639d7abf
JULIO CESAR MARTINI <julio.martini@alunos.sc.senac.br>
2024-11-04 19:25:49 -0300

    Atualizaçao da classe divisao e integraçao com a interface

--- SuperCalculadora/SuperCalculadora/obj/Debug/net6.0/SuperCalculadora.AssemblyInfo.cs
+++ SuperCalculadora/SuperCalculadora/obj/Debug/net6.0/SuperCalculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperCalculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bcf87b5fa17ea7a3f94731ffa53f2326a39bc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperCalculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperCalculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
